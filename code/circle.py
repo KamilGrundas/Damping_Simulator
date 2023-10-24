a5eb57577c13e2af08216f977cca60e5289c9135
Kamil Grundas <kamil.grundas@gmail.com>
2023-10-24 22:14:34 +0200

    Added shape and movement for circle

--- code/circle.py
+++ code/circle.py
@@ -7,6 +7,27 @@ class Circle(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
-        self.image = pygame.Surface((32,64))
-        self.image.fill("green")
-        self.rect = self.image.get_rect(center = pos)
+        # Create an image surface with a transparent background
+        self.image = pygame.Surface((64, 64), pygame.SRCALPHA)
+        
+        # Draw a green circle on the image surface
+        pygame.draw.circle(self.image, (0, 255, 0), (32, 32), 32)
+
+        # Set the position of the sprite
+        self.rect = self.image.get_rect(center=pos)
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 100
+
+    def move(self,dt):
+
+        self.pos.x += 1 * self.speed * dt
+
+        self.rect.centerx = self.pos.x
+
+        self.pos.y += 1 * self.speed * dt
+
+        self.rect.centery = self.pos.y
+
+
+    def update(self,dt):
+        self.move(dt)
